ad3161f780445fe7e64f2a69ca8180959d1b97f3
Aaryan Jaiswal <68491979+AaryanJ8@users.noreply.github.com>
2023-09-17 23:00:27 +0530

    SwiftUI iOS Project created

---
+++ iOSApp/NotesApp/NotesApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NotesApp
+//
+//  Created by Aaryan jaiswal on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello Hello Hello")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSApp/NotesApp/NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Aaryan jaiswal on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
